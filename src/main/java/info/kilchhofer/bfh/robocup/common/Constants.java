63167d6b85cbe53d42843abfdebbc73b9741dd2b
Marco Kilchhofer <marco@kilchhofer.info>
2018-06-07 09:37:01 +0200

    Add LoggerSettings and move Constants

--- src/main/java/info/kilchhofer/bfh/robocup/common/Constants.java
+++ src/main/java/info/kilchhofer/bfh/robocup/common/Constants.java
@@ -1,7 +1,7 @@
-package info.kilchhofer.bfh.lidar;
+package info.kilchhofer.bfh.robocup.common;
 
 public class Constants {
-    public static final String LIDAR_ROOT_CONTEXT = "Robocup";
+    public static final String ROBOCUP_ROOT_CONTEXT = "Robocup";
     public static final String DEFAULT_BROKER_ADDRESS = "tcp://127.0.0.1:1883";
 
 }
